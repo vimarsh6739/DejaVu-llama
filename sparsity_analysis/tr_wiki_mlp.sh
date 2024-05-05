e8daeaa70216f7b976e5bf07cec16068acffae4f
Vimarsh Sathia <vsathia2@illinois.edu>
2024-05-05 12:09:25 -0500

    fix training bug in script

--- sparsity_analysis/tr_wiki_mlp.sh
+++ sparsity_analysis/tr_wiki_mlp.sh
@@ -4,5 +4,5 @@ DATA_PATH="/shared/vsathia2/sp_mlp_predictor/$1"
 
 for l in $(seq 0 31) 
 do  
-python main_mlp.py --dataset wikitext --lr 0.001 --L ${l} > ${DATA_PATH}/wiki_mlp_out_${l}.txt  
+python main_mlp.py --dataset wikitext --lr 0.001 --L ${l} --model ${1} > ${DATA_PATH}/wiki_mlp_out_${l}.txt  
 done
